e725ac6c83e60c08a0cc6dbbae5cfd989e1b0ab5
曾先生 <zeng@example.com>
2026-09-19 08:58:14 +0800

    fix(ios): fix AppDelegate universal link handler for swift-pm compatibility

--- client/ios/App/App/AppDelegate.swift
+++ client/ios/App/App/AppDelegate.swift
@@ -41,9 +41,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
         // Called when the app was launched with an activity, including Universal Links.
-        // Feel free to add additional processing here, but if you want the App API to support
-        // tracking app url opens, make sure to keep this call
-        return ApplicationDelegateProxy.shared.application(application, continue: userActivity, restorationHandler: restorationHandler)
+        if userActivity.activityType == NSUserActivityTypeBrowsingWeb, let url = userActivity.webpageURL {
+            NotificationCenter.default.post(name: Notification.Name("capacitorOpenUniversalLink"), object: ["url": url])
+            return true
+        }
+        return true
     }
 
 }
